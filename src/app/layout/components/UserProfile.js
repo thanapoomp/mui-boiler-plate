e46f0b3a53611b3eae49a8fc47d7059d074bd811
go <go.nobpadon25@gmail.com>
2021-07-12 14:56:51 +0700

    add default pic user

--- src/app/layout/components/UserProfile.js
+++ src/app/layout/components/UserProfile.js
@@ -3,9 +3,18 @@ import React from 'react'
 import { Typography } from '@material-ui/core'
 import { useSelector } from "react-redux";
 import { Grid } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
 
 
 function UserProfile() {
+	const useStyle = makeStyles((theme) => ({
+		image: {
+			width: 50,
+			height: 50,
+		},
+	}));
+	const classes = useStyle();
 	const authReducer = useSelector(({ auth }) => auth)
 	return (
 		<div>
@@ -15,6 +24,13 @@ function UserProfile() {
 				justifyContent="flex-start"
 				alignItems="center"
 			>
+				<Paper elevation={0} style={{ marginRight: 5, padding: 5 }}>
+					<img
+						className={classes.image}
+						alt=""
+						src={process.env.PUBLIC_URL + "/default.jpg"}
+					/>
+				</Paper>
 				<Typography variant="h6" style={{ color: "#847b7b" }}>User: </Typography>
 				<Typography variant="h6" style={{ marginLeft: 5, color: "#000000" }}>{authReducer.user}</Typography>
 			</Grid>
